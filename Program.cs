762c433fb1dae6947826e0130c493c922dcaaab7
GustavoFMarcial <gfm211101@gmail.com>
2025-08-07 21:56:16 -0300

    Estudo

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            Client c1 = new Client("Gustavo", "999999999", "gustavo@gmail.com", "R. dos Catapimbas, bairro Joba, 123 - Cuiabá/MT");
+            Client gustavo = new Client("Gustavo", "999999999", "gustavo@gmail.com", "R. dos Catapimbas, bairro Joba, 123 - Cuiabá/MT");
             List<IItem> items = [new Fries(), new Hamburger(), new Soda(), new Cookie()];
             IOrderType delivery = new Delivery();
             INotificationSystem message = new MessageSender();
             IPaymentType pix = new Pix();
 
-            Order o1 = new Order(c1, items, delivery, message, pix);
+            Order o1 = new Order(gustavo, items, delivery, message, pix);
 
             Console.WriteLine(o1.OrderType.Type);
             o1.PaymentType.ProcessPayment();
